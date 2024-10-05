190f1363d80752f1a2fd2e8c49b627a9f7c2879c
henrypaul2001 <henryp1.hp@googlemail.com>
2024-10-05 16:54:07 +0100

    Implemented anti aliasing with multi sampling

--- RayTracingInOneWeekendSeries/camera.h
+++ RayTracingInOneWeekendSeries/camera.h
@@ -3,8 +3,9 @@
 
 class camera {
 public:
-	float aspect_ratio = 1.0f; // Ratio of image width over height
-	int image_width = 100;	   // Rendered image width in pixel count
+	float aspect_ratio = 1.0f;	// Ratio of image width over height
+	int image_width = 100;	    // Rendered image width in pixel count
+	int samples_per_pixel = 10; // Number of random samples per pixel
 
 	void render(const hittable& world) {
 		initialize();
@@ -13,12 +14,12 @@ public:
 		for (int j = 0; j < image_height; j++) {
 			std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 			for (int i = 0; i < image_width; i++) {
-				point3 pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-				vec3 ray_direction = pixel_center - center;
-				ray r = ray(center, ray_direction);
-
-				colour pixel_colour = ray_colour(r, world);
-				WriteColour(std::cout, pixel_colour);
+				colour pixel_colour = colour(0.0f);
+				for (int sample = 0; sample < samples_per_pixel; sample++) {
+					ray r = get_ray(i, j);
+					pixel_colour += ray_colour(r, world);
+				}
+				WriteColour(std::cout, pixel_samples_scale * pixel_colour);
 			}
 		}
 
@@ -26,15 +27,18 @@ public:
 	}
 
 private:
-	int image_height;    // Rendered image height
-	point3 center;       // Camera center
-	point3 pixel00_loc;  // Location of pixel 0, 0
-	vec3 pixel_delta_u;  // Offset to pixel to the right
-	vec3 pixel_delta_v;  // Offset to pixel below
+	int image_height;			// Rendered image height
+	float pixel_samples_scale;  // Colour scale factor for sum of pixel samples
+	point3 center;				// Camera center
+	point3 pixel00_loc;			// Location of pixel 0, 0
+	vec3 pixel_delta_u;			// Offset to pixel to the right
+	vec3 pixel_delta_v;			// Offset to pixel below
 
 	void initialize() {
 		image_height = int(image_width / aspect_ratio);
 		image_height = (image_height < 1) ? 1 : image_height;
+
+		pixel_samples_scale = 1.0f / samples_per_pixel;
 
 		center = point3(0, 0, 0);
 
@@ -54,6 +58,24 @@ private:
 		// Upper left pixel location
 		auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
 		pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+	}
+
+	ray get_ray(int i, int j) const {
+		// Construct camera ray originating from origin and directed to random sample
+		vec3 offset = sample_square();
+		vec3 pixel_sample = pixel00_loc
+			+ ((i + offset.x()) * pixel_delta_u)
+			+ ((j + offset.y()) * pixel_delta_v);
+
+		point3 ray_origin = center;
+		vec3 ray_direction = pixel_sample - ray_origin;
+
+		return ray(ray_origin, ray_direction);
+	}
+
+	vec3 sample_square() const {
+		// Returns vector to a random point in the -.5, -.5 to .5, .5 square
+		return vec3(random_double() - 0.5f, random_double() - 0.5f, 0.0f);
 	}
 
 	colour ray_colour(const ray& r, const hittable& world) const {
--- RayTracingInOneWeekendSeries/colour.h
+++ RayTracingInOneWeekendSeries/colour.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "vec3.h"
+#include "interval.h"
 using colour = vec3;
 inline void WriteColour(std::ostream& out, const colour& rgbColour) {
 	float r = rgbColour.r();
