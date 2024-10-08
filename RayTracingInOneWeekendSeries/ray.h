2ffc31b8d1a2aac880d64bbc5892bb122d51d849
henrypaul2001 <henryp1.hp@googlemail.com>
2024-10-08 14:08:52 +0100

    Added time value to ray class

--- RayTracingInOneWeekendSeries/ray.h
+++ RayTracingInOneWeekendSeries/ray.h
@@ -4,10 +4,13 @@
 class ray {
 public:
 	ray() {}
+	ray(const point3& origin, const vec3& direction, const float time) : origin(origin), direction(direction), tm(time) {}
 	ray(const point3& origin, const vec3& direction) : origin(origin), direction(direction) {}
 
 	const point3& Origin() const { return origin; }
 	const vec3& Direction() const { return direction; }
+
+	const float time() const { return tm; }
 
 	point3 at(float t) const {
 		return origin + (t * direction);
@@ -16,4 +19,5 @@ public:
 private:
 	point3 origin;
 	vec3 direction;
+	float tm;
 };
