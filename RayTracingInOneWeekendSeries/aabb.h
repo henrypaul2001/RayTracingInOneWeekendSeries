0b9a309e1d9c22f87ef9719fea6ac42366b8c7ab
henrypaul2001 <henryp1.hp@googlemail.com>
2024-10-08 14:51:55 +0100

    Created aabb class

---
+++ RayTracingInOneWeekendSeries/aabb.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "interval.h"
+#include "vec3.h"
+#include "ray.h"
+
+class aabb {
+public:
+	interval x, y, z;
+
+	aabb() {}
+	aabb(const interval& x, const interval& y, const interval& z) : x(x), y(y), z(z) {}
+	aabb(const point3& a, const point3& b) {
+		x = (a[0] <= b[0]) ? interval(a[0], b[0]) : interval(b[0], a[0]);
+		y = (a[1] <= b[1]) ? interval(a[1], b[1]) : interval(b[1], a[1]);
+		z = (a[2] <= b[2]) ? interval(a[2], b[2]) : interval(b[2], a[2]);
+	}
+
+	const interval& axis_interval(const int n) const {
+		if (n == 1) return y;
+		if (n == 2) return z;
+		return x;
+	}
+
+	bool hit(const ray& r, interval ray_t) const {
+		const point3& ray_orig = r.Origin();
+		const vec3& ray_dir = r.Direction();
+
+		for (int axis = 0; axis < 3; axis++) {
+			const interval& ax = axis_interval(axis);
+			const float adinv = 1.0f / ray_dir[axis];
+
+			float t0 = (ax.min - ray_orig[axis]) * adinv;
+			float t1 = (ax.max - ray_orig[axis]) * adinv;
+
+			if (t0 < t1) {
+				if (t0 > ray_t.min) { ray_t.min = t0; }
+				if (t1 < ray_t.max) { ray_t.max = t1; }
+			}
+			else {
+				if (t1 > ray_t.min) { ray_t.min = t1; }
+				if (t0 < ray_t.max) { ray_t.max = t0; }
+			}
+
+			if (ray_t.max <= ray_t.min) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+};
--- RayTracingInOneWeekendSeries/interval.h
+++ RayTracingInOneWeekendSeries/interval.h
@@ -7,12 +7,21 @@ public:
 	interval(const float min, const float max) : min(min), max(max) {}
 
 	float size() const { return max - min; }
+
 	bool contains(const float x) const { return min <= x && x <= max; }
+
 	bool surrounds(const float x) const { return min < x && x < max; }
+
 	float clamp(const float x) const { 
 		if (x < min) { return min; }
 		if (x > max) { return max; }
 		return x;
 	}
+
+	interval expand(const float delta) const {
+		float padding = delta / 2.0f;
+		return interval(min - padding, max + padding);
+	}
+
 	static const interval empty, universe;
 };
