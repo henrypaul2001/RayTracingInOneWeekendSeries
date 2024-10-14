327707ac0537f8dbd3d74eb4ee74407a848dfb5a
henrypaul2001 <henryp1.hp@googlemail.com>
2024-10-14 15:49:36 +0100

    Created orthonormal basis class

---
+++ RayTracingInOneWeekendSeries/onb.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "vec3.h"
+
+// Orthonormal basis
+class onb {
+public:
+	onb(const vec3& n) {
+		axis[2] = normalize(n);
+		vec3 a = (std::fabs(axis[2].x()) > 0.9f) ? vec3(0.0f, 1.0f, 0.0f) : vec3(1.0f, 0.0f, 0.0f);
+		axis[1] = normalize(cross(axis[2], a));
+		axis[0] = cross(axis[2], axis[1]);
+	}
+
+	const vec3& u() const { return axis[0]; }
+	const vec3& v() const { return axis[1]; }
+	const vec3& w() const { return axis[2]; }
+
+	vec3 transform(const vec3& v) const {
+		// Transform from basis coordinates to local space
+		return (v[0] * axis[0]) + (v[1] * axis[1]) + (v[2] * axis[2]);
+	}
+
+private:
+	vec3 axis[3];
+};
